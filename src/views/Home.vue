0679aaf27bf8fb5cb089b3c205f0223e068a4b4a
Sebastian Farias <sfarias.arellano@gmail.com>
2020-09-25 11:38:22 -0300

    avanzada

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,42 +3,44 @@
 		<header>
 			<div class="banner"> </div>
 		</header>
-		<section class="looking">
-			<div class="container">
-				<h3>¿Que auto compro?</h3>
-				<p>
-					¿Te gustaría tener ese modelito de auto que tanto has visto? <br />
-					Pon atención a este artículo, puesto que al momento <br />
-					de comprar un auto usado, hay varias cosas que debes saber, además de <br />
-					realizar algunos trámites. En el blog de <br />
-					LegalChile te lo contamos. <br />
-					Foto: La Tercera
-				</p>
+ <section class="looking">
+      <div class="container">
+        <h3>Tipos de Vehiculos</h3>
+        <p></p>
 
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
-					Possimus provident iste repudiandae numquam accusamus <br />
-					laborum ratione doloribus quibusdam? Asperiores possimus <br />
-					aspernatur modi aut voluptatum. Odit illum quidem corrupti <br />
-					aliquid ullam?
-				</p>
-			</div>
-				<div class="card" style="width: 18rem;">
-				<strong><p>¿ENCUENTRAS EL INDICADO?</p></strong>
-				<div class="card-body">
-					<h5 class="card-title">¿Quieres comprar un vehículo?</h5>
-					<p class="card-text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ea culpa fugit ipsam corporis
-						suscipit commodi itaque, doloribus necessitatibus...
-					</p>
-					<a
-						href="https://www.primeauxmitsubishi.com/"
-						
-						>Loockea Ofertas!</a
-					>
-				</div>
-			</div>
-		</section>
+        <p>
+          Los monovolúmenes son un tipo de coche según su carrocería. El espacio
+          interior integra en un solo volumen el compartimento de equipajes, el
+          de pasajeros y el del motor. El motor está alojado de tal manera que
+          sobresale muy poco por delante del eje delantero, y está montado más
+          alto e incluso más inclinado que en un turismo, para liberar espacio
+          en el habitáculo. Atendiendo a este volumen interior, existen otras
+          variantes: dos volúmenes (maletero y habitáculo están comunicados) y
+          tres volúmenes (el espacio del maletero y el motor están separados).
+        </p>
+      </div>
+      <div class="card" style="width: 18rem">
+        <strong><p>¿ENCUENTRAS EL INDICADO?</p></strong>
+        <div class="card-body">
+          <h5 class="card-title">¿Quieres comprar un vehículo?</h5>
+          <h6>SIGUE ESTOS TIPS AL MOMENTO DE COMPRAR</h6>
+          <p class="card-text">
+            Toca molduras, parrilla y defensas, para detectar si existió algún
+            daño y quedó mal reparado. Revisa si existen abolladuras, rayones y
+            óxido en todos los paneles de carrocería y techo. Ten en cuenta que
+            la separación que existe entre los paneles y las superficies que los
+            rodean tiene que ser uniforme: si están mal alineados u observas una
+            separación desmesurada, puede que tengan una falla de fábrica o
+            hayan sido reparados en alguna oportunidad.
+          </p>
+		  <p>¿Quieres saber más?</p>
+          <a
+            href="https://www.autofact.cl/blog/comprar-auto/revision/revisar-exterior-auto"
+            >Loockea Ofertas!</a
+          >
+        </div>
+      </div>
+    </section>
 
 		<footer>
 <div class="Footer">
